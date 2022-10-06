8b138a6ee7652ab655191ccabf444b38bbf80286
ctangney-tulip <107075008+ctangney-tulip@users.noreply.github.com>
2022-10-06 10:37:49 -0400

    Add @rollup/plugin-alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,26 @@
 import { defineConfig } from 'vite';
+import alias from '@rollup/plugin-alias';
 import path from 'path';
 
 export default defineConfig({
+  plugins: [
+    alias({
+      entries: [
+        {
+          find: '@components',
+          replacement: `${path.resolve(__dirname, './src/components/')}`,
+        },
+        {
+          find: '@styles',
+          replacement: path.resolve(__dirname, './src/styles/'),
+        },
+        {
+          find: '@utils',
+          replacement: path.resolve(__dirname, './src/utils/'),
+        },
+      ],
+    }),
+  ],
   resolve: {
     alias: {
       '@components': path.resolve(__dirname, './src/components/'),
